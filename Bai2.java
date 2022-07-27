0256d377a0b5799e0b4e164e4d6e03bfba8d408b
thidiepdangiu <108882361+thidiepdangiu@users.noreply.github.com>
2022-07-27 16:27:24 +0700

    Update Bai2.java

--- Bai2.java
+++ Bai2.java
@@ -4,6 +4,7 @@ public class Bai2 {
 	public static void main(String[] args) {
 		String s;
 		String s1 = "";
+		String s2 = "";
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Mời nhập vào một chuỗi: ");
 		s = sc.nextLine();
@@ -14,14 +15,13 @@ public class Bai2 {
 		
 		int n = s.length();
 		for (int i = 0; i < n; i++) {
-			if ((int)s.charAt(i) >= 48 && (int)s.charAt(i) <= 57) {
-				s=xoakt(s, i);//xóa kí tự số
-				i--;
-				n--;
-			}
+			if (s.charAt(i) >= '0' && s.charAt(i) <= '9') {
+				s2 = s2 + "";
+			} else
+				s2 = s2 + s.charAt(i);
 		}
 		
-		String d[] = s.split(" ");
+		String d[] = s2.split(" ");
 		for (int i = 0; i < d.length; i++) {
 			if (d[i].length() != 0) {
 				s1 += d[i].substring(0, 1).toUpperCase() + d[i].substring(1) + " ";//viết hoa chữ cái đầu tiên
@@ -29,9 +29,5 @@ public class Bai2 {
 		}
 		System.out.println("\n");
 		System.out.println("Chuỗi sau khi chuẩn hóa là: " + s1);
-	}
-
-	public static String xoakt(String s, int k) {
-		return s.substring(0, k) + s.substring(k + 1);
 	}
 }
